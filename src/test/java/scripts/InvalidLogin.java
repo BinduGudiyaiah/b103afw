914f10482be4a799c158beb659630c42f4d32d0e
BinduGudiyaiah <bindugudiyaiah@gmail.com>
2024-07-10 08:03:28 +0530

    Added Invalid Login

---
+++ src/test/java/scripts/InvalidLogin.java
@@ -0,0 +1,33 @@
+package scripts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+import generic.Utility;
+import page.LoginPage;
+
+public class InvalidLogin extends BaseTest
+{
+	@Test(priority = 2)
+	public void testInvalidLogin()
+	{
+		String un = Utility.getExcelData(EXCEL_PATH, "ValidLogin",2, 0);
+		String pw = Utility.getExcelData(EXCEL_PATH, "ValidLogin",2, 1);
+		
+//		1. enter invalid user name
+		LoginPage loginPage=new LoginPage(driver);
+		loginPage.setUserName(un);
+		
+//		2. enter invalid password
+		loginPage.setPassword(pw);
+		
+//		3. click on go button
+		loginPage.clickGoButton();
+		
+//		4. verify err msg is displayed
+		boolean result = loginPage.verifyErrMsgIsDisplayed(wait);
+		Assert.assertTrue(result);
+		
+	}
+}
